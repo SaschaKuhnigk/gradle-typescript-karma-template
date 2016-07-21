cef5eee0ee73a4a47789a7d494445d37f28c53ec
Sascha Kuhnigk <sascha.kuhnigk@gmx.de>
2016-07-21 19:57:04 +0200

    initial build

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+module.exports = function (config) {
+    config.set({
+                   basePath: '',
+                   frameworks: ['jasmine'],
+                   browsers: ['PhantomJS', 'Chrome'],
+                   logLevel: config.LOG_DEBUG,
+                   protocol: 'http',
+                   plugins: [
+                       'karma-jasmine',
+                       'karma-phantomjs-launcher',
+                       'karma-chrome-launcher',
+                       'karma-sourcemap-loader'
+                   ],
+                   port: 9876,
+                   autoWatch: true,
+
+                   files: [
+                       {pattern: './build/generated-source/**/*.js', included: true},
+                       {pattern: './build/generated-source/**/*.js.map', included: false}
+                   ],
+                   preprocessors: {
+                       './build/generated-source/**/ts/*.js': ['sourcemap']
+                   },
+                   commonjsPreprocessor: {
+                       modulesRoot: './build/generated-source/test/ts/'
+                   }
+               });
+};
---
+++ src/test/ts/GreeterUnitTest.ts
@@ -0,0 +1,10 @@
+describe("A suite", function () {
+
+    it("test1", function () {
+        expect("Hello, foo").toBe("Hello, foo");
+    });
+
+    it("test2", function () {
+        expect(true).toBe(true);
+    });
+});
