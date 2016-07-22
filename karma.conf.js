4f03244c3264d0ac637195850070c042a52cdca3
Sascha Kuhnigk <sascha.kuhnigk@gmx.de>
2016-07-22 19:48:43 +0200

    integrate requirejs

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine','requirejs'],
         browsers: ['PhantomJS', 'Chrome'],
         logLevel: config.LOG_DEBUG,
         protocol: 'http',
@@ -9,14 +9,16 @@ module.exports = function (config) {
             'karma-jasmine',
             'karma-phantomjs-launcher',
             'karma-chrome-launcher',
-            'karma-sourcemap-loader'
+            'karma-sourcemap-loader',
+            'karma-requirejs'
         ],
         port: 9876,
         autoWatch: true,
 
         files: [
-            {pattern: './build/generated-source/**/*.js', included: true},
-            {pattern: './build/generated-source/**/*.js.map', included: false}
+            {pattern: './build/generated-source/**/*.js', included: false},
+            {pattern: './build/generated-source/**/*.js.map', included: false},
+            'src/test/ts/test-main.js'
         ],
         preprocessors: {
             './build/generated-source/**/ts/*.js': ['sourcemap']
